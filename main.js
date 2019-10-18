070e132850acbe6dd3ca80568d7147d124ebf37d
Charles <charles081889@gmail.com>
2019-10-18 20:28:25 +0100

    worked on cart functionalities

--- main.js
+++ main.js
@@ -1,248 +1,270 @@
-const countries = [
-    {"name": "Afghanistan", "code": "AF"}, 
-    {"name": "land Islands", "code": "AX"}, 
-    {"name": "Albania", "code": "AL"}, 
-    {"name": "Algeria", "code": "DZ"}, 
-    {"name": "American Samoa", "code": "AS"}, 
-    {"name": "AndorrA", "code": "AD"}, 
-    {"name": "Angola", "code": "AO"}, 
-    {"name": "Anguilla", "code": "AI"}, 
-    {"name": "Antarctica", "code": "AQ"}, 
-    {"name": "Antigua and Barbuda", "code": "AG"}, 
-    {"name": "Argentina", "code": "AR"}, 
-    {"name": "Armenia", "code": "AM"}, 
-    {"name": "Aruba", "code": "AW"}, 
-    {"name": "Australia", "code": "AU"}, 
-    {"name": "Austria", "code": "AT"}, 
-    {"name": "Azerbaijan", "code": "AZ"}, 
-    {"name": "Bahamas", "code": "BS"}, 
-    {"name": "Bahrain", "code": "BH"}, 
-    {"name": "Bangladesh", "code": "BD"}, 
-    {"name": "Barbados", "code": "BB"}, 
-    {"name": "Belarus", "code": "BY"}, 
-    {"name": "Belgium", "code": "BE"}, 
-    {"name": "Belize", "code": "BZ"}, 
-    {"name": "Benin", "code": "BJ"}, 
-    {"name": "Bermuda", "code": "BM"}, 
-    {"name": "Bhutan", "code": "BT"}, 
-    {"name": "Bolivia", "code": "BO"}, 
-    {"name": "Bosnia and Herzegovina", "code": "BA"}, 
-    {"name": "Botswana", "code": "BW"}, 
-    {"name": "Bouvet Island", "code": "BV"}, 
-    {"name": "Brazil", "code": "BR"}, 
-    {"name": "British Indian Ocean Territory", "code": "IO"}, 
-    {"name": "Brunei Darussalam", "code": "BN"}, 
-    {"name": "Bulgaria", "code": "BG"}, 
-    {"name": "Burkina Faso", "code": "BF"}, 
-    {"name": "Burundi", "code": "BI"}, 
-    {"name": "Cambodia", "code": "KH"}, 
-    {"name": "Cameroon", "code": "CM"}, 
-    {"name": "Canada", "code": "CA"}, 
-    {"name": "Cape Verde", "code": "CV"}, 
-    {"name": "Cayman Islands", "code": "KY"}, 
-    {"name": "Central African Republic", "code": "CF"}, 
-    {"name": "Chad", "code": "TD"}, 
-    {"name": "Chile", "code": "CL"}, 
-    {"name": "China", "code": "CN"}, 
-    {"name": "Christmas Island", "code": "CX"}, 
-    {"name": "Cocos (Keeling) Islands", "code": "CC"}, 
-    {"name": "Colombia", "code": "CO"}, 
-    {"name": "Comoros", "code": "KM"}, 
-    {"name": "Congo", "code": "CG"}, 
-    {"name": "Congo, The Democratic Republic of the", "code": "CD"}, 
-    {"name": "Cook Islands", "code": "CK"}, 
-    {"name": "Costa Rica", "code": "CR"}, 
-    {"name": "Cote D'Ivoire", "code": "CI"}, 
-    {"name": "Croatia", "code": "HR"}, 
-    {"name": "Cuba", "code": "CU"}, 
-    {"name": "Cyprus", "code": "CY"}, 
-    {"name": "Czech Republic", "code": "CZ"}, 
-    {"name": "Denmark", "code": "DK"}, 
-    {"name": "Djibouti", "code": "DJ"}, 
-    {"name": "Dominica", "code": "DM"}, 
-    {"name": "Dominican Republic", "code": "DO"}, 
-    {"name": "Ecuador", "code": "EC"}, 
-    {"name": "Egypt", "code": "EG"}, 
-    {"name": "El Salvador", "code": "SV"}, 
-    {"name": "Equatorial Guinea", "code": "GQ"}, 
-    {"name": "Eritrea", "code": "ER"}, 
-    {"name": "Estonia", "code": "EE"}, 
-    {"name": "Ethiopia", "code": "ET"}, 
-    {"name": "Falkland Islands (Malvinas)", "code": "FK"}, 
-    {"name": "Faroe Islands", "code": "FO"}, 
-    {"name": "Fiji", "code": "FJ"}, 
-    {"name": "Finland", "code": "FI"}, 
-    {"name": "France", "code": "FR"}, 
-    {"name": "French Guiana", "code": "GF"}, 
-    {"name": "French Polynesia", "code": "PF"}, 
-    {"name": "French Southern Territories", "code": "TF"}, 
-    {"name": "Gabon", "code": "GA"}, 
-    {"name": "Gambia", "code": "GM"}, 
-    {"name": "Georgia", "code": "GE"}, 
-    {"name": "Germany", "code": "DE"}, 
-    {"name": "Ghana", "code": "GH"}, 
-    {"name": "Gibraltar", "code": "GI"}, 
-    {"name": "Greece", "code": "GR"}, 
-    {"name": "Greenland", "code": "GL"}, 
-    {"name": "Grenada", "code": "GD"}, 
-    {"name": "Guadeloupe", "code": "GP"}, 
-    {"name": "Guam", "code": "GU"}, 
-    {"name": "Guatemala", "code": "GT"}, 
-    {"name": "Guernsey", "code": "GG"}, 
-    {"name": "Guinea", "code": "GN"}, 
-    {"name": "Guinea-Bissau", "code": "GW"}, 
-    {"name": "Guyana", "code": "GY"}, 
-    {"name": "Haiti", "code": "HT"}, 
-    {"name": "Heard Island and Mcdonald Islands", "code": "HM"}, 
-    {"name": "Holy See (Vatican City State)", "code": "VA"}, 
-    {"name": "Honduras", "code": "HN"}, 
-    {"name": "Hong Kong", "code": "HK"}, 
-    {"name": "Hungary", "code": "HU"}, 
-    {"name": "Iceland", "code": "IS"}, 
-    {"name": "India", "code": "IN"}, 
-    {"name": "Indonesia", "code": "ID"}, 
-    {"name": "Iran, Islamic Republic Of", "code": "IR"}, 
-    {"name": "Iraq", "code": "IQ"}, 
-    {"name": "Ireland", "code": "IE"}, 
-    {"name": "Isle of Man", "code": "IM"}, 
-    {"name": "Israel", "code": "IL"}, 
-    {"name": "Italy", "code": "IT"}, 
-    {"name": "Jamaica", "code": "JM"}, 
-    {"name": "Japan", "code": "JP"}, 
-    {"name": "Jersey", "code": "JE"}, 
-    {"name": "Jordan", "code": "JO"}, 
-    {"name": "Kazakhstan", "code": "KZ"}, 
-    {"name": "Kenya", "code": "KE"}, 
-    {"name": "Kiribati", "code": "KI"}, 
-    {"name": "Korea, Democratic People'S Republic of", "code": "KP"}, 
-    {"name": "Korea, Republic of", "code": "KR"}, 
-    {"name": "Kuwait", "code": "KW"}, 
-    {"name": "Kyrgyzstan", "code": "KG"}, 
-    {"name": "Lao People'S Democratic Republic", "code": "LA"}, 
-    {"name": "Latvia", "code": "LV"}, 
-    {"name": "Lebanon", "code": "LB"}, 
-    {"name": "Lesotho", "code": "LS"}, 
-    {"name": "Liberia", "code": "LR"}, 
-    {"name": "Libyan Arab Jamahiriya", "code": "LY"}, 
-    {"name": "Liechtenstein", "code": "LI"}, 
-    {"name": "Lithuania", "code": "LT"}, 
-    {"name": "Luxembourg", "code": "LU"}, 
-    {"name": "Macao", "code": "MO"}, 
-    {"name": "Macedonia, The Former Yugoslav Republic of", "code": "MK"}, 
-    {"name": "Madagascar", "code": "MG"}, 
-    {"name": "Malawi", "code": "MW"}, 
-    {"name": "Malaysia", "code": "MY"}, 
-    {"name": "Maldives", "code": "MV"}, 
-    {"name": "Mali", "code": "ML"}, 
-    {"name": "Malta", "code": "MT"}, 
-    {"name": "Marshall Islands", "code": "MH"}, 
-    {"name": "Martinique", "code": "MQ"}, 
-    {"name": "Mauritania", "code": "MR"}, 
-    {"name": "Mauritius", "code": "MU"}, 
-    {"name": "Mayotte", "code": "YT"}, 
-    {"name": "Mexico", "code": "MX"}, 
-    {"name": "Micronesia, Federated States of", "code": "FM"}, 
-    {"name": "Moldova, Republic of", "code": "MD"}, 
-    {"name": "Monaco", "code": "MC"}, 
-    {"name": "Mongolia", "code": "MN"}, 
-    {"name": "Montenegro", "code": "ME"},
-    {"name": "Montserrat", "code": "MS"},
-    {"name": "Morocco", "code": "MA"}, 
-    {"name": "Mozambique", "code": "MZ"}, 
-    {"name": "Myanmar", "code": "MM"}, 
-    {"name": "Namibia", "code": "NA"}, 
-    {"name": "Nauru", "code": "NR"}, 
-    {"name": "Nepal", "code": "NP"}, 
-    {"name": "Netherlands", "code": "NL"}, 
-    {"name": "Netherlands Antilles", "code": "AN"}, 
-    {"name": "New Caledonia", "code": "NC"}, 
-    {"name": "New Zealand", "code": "NZ"}, 
-    {"name": "Nicaragua", "code": "NI"}, 
-    {"name": "Niger", "code": "NE"}, 
-    {"name": "Nigeria", "code": "NG"}, 
-    {"name": "Niue", "code": "NU"}, 
-    {"name": "Norfolk Island", "code": "NF"}, 
-    {"name": "Northern Mariana Islands", "code": "MP"}, 
-    {"name": "Norway", "code": "NO"}, 
-    {"name": "Oman", "code": "OM"}, 
-    {"name": "Pakistan", "code": "PK"}, 
-    {"name": "Palau", "code": "PW"}, 
-    {"name": "Palestinian Territory, Occupied", "code": "PS"}, 
-    {"name": "Panama", "code": "PA"}, 
-    {"name": "Papua New Guinea", "code": "PG"}, 
-    {"name": "Paraguay", "code": "PY"}, 
-    {"name": "Peru", "code": "PE"}, 
-    {"name": "Philippines", "code": "PH"}, 
-    {"name": "Pitcairn", "code": "PN"}, 
-    {"name": "Poland", "code": "PL"}, 
-    {"name": "Portugal", "code": "PT"}, 
-    {"name": "Puerto Rico", "code": "PR"}, 
-    {"name": "Qatar", "code": "QA"}, 
-    {"name": "Reunion", "code": "RE"}, 
-    {"name": "Romania", "code": "RO"}, 
-    {"name": "Russian Federation", "code": "RU"}, 
-    {"name": "RWANDA", "code": "RW"}, 
-    {"name": "Saint Helena", "code": "SH"}, 
-    {"name": "Saint Kitts and Nevis", "code": "KN"}, 
-    {"name": "Saint Lucia", "code": "LC"}, 
-    {"name": "Saint Pierre and Miquelon", "code": "PM"}, 
-    {"name": "Saint Vincent and the Grenadines", "code": "VC"}, 
-    {"name": "Samoa", "code": "WS"}, 
-    {"name": "San Marino", "code": "SM"}, 
-    {"name": "Sao Tome and Principe", "code": "ST"}, 
-    {"name": "Saudi Arabia", "code": "SA"}, 
-    {"name": "Senegal", "code": "SN"}, 
-    {"name": "Serbia", "code": "RS"}, 
-    {"name": "Seychelles", "code": "SC"}, 
-    {"name": "Sierra Leone", "code": "SL"}, 
-    {"name": "Singapore", "code": "SG"}, 
-    {"name": "Slovakia", "code": "SK"}, 
-    {"name": "Slovenia", "code": "SI"}, 
-    {"name": "Solomon Islands", "code": "SB"}, 
-    {"name": "Somalia", "code": "SO"}, 
-    {"name": "South Africa", "code": "ZA"}, 
-    {"name": "South Georgia and the South Sandwich Islands", "code": "GS"}, 
-    {"name": "Spain", "code": "ES"}, 
-    {"name": "Sri Lanka", "code": "LK"}, 
-    {"name": "Sudan", "code": "SD"}, 
-    {"name": "Suriname", "code": "SR"}, 
-    {"name": "Svalbard and Jan Mayen", "code": "SJ"}, 
-    {"name": "Swaziland", "code": "SZ"}, 
-    {"name": "Sweden", "code": "SE"}, 
-    {"name": "Switzerland", "code": "CH"}, 
-    {"name": "Syrian Arab Republic", "code": "SY"}, 
-    {"name": "Taiwan, Province of China", "code": "TW"}, 
-    {"name": "Tajikistan", "code": "TJ"}, 
-    {"name": "Tanzania, United Republic of", "code": "TZ"}, 
-    {"name": "Thailand", "code": "TH"}, 
-    {"name": "Timor-Leste", "code": "TL"}, 
-    {"name": "Togo", "code": "TG"}, 
-    {"name": "Tokelau", "code": "TK"}, 
-    {"name": "Tonga", "code": "TO"}, 
-    {"name": "Trinidad and Tobago", "code": "TT"}, 
-    {"name": "Tunisia", "code": "TN"}, 
-    {"name": "Turkey", "code": "TR"}, 
-    {"name": "Turkmenistan", "code": "TM"}, 
-    {"name": "Turks and Caicos Islands", "code": "TC"}, 
-    {"name": "Tuvalu", "code": "TV"}, 
-    {"name": "Uganda", "code": "UG"}, 
-    {"name": "Ukraine", "code": "UA"}, 
-    {"name": "United Arab Emirates", "code": "AE"}, 
-    {"name": "United Kingdom", "code": "GB"}, 
-    {"name": "United States", "code": "US"}, 
-    {"name": "United States Minor Outlying Islands", "code": "UM"}, 
-    {"name": "Uruguay", "code": "UY"}, 
-    {"name": "Uzbekistan", "code": "UZ"}, 
-    {"name": "Vanuatu", "code": "VU"}, 
-    {"name": "Venezuela", "code": "VE"}, 
-    {"name": "Viet Nam", "code": "VN"}, 
-    {"name": "Virgin Islands, British", "code": "VG"}, 
-    {"name": "Virgin Islands, U.S.", "code": "VI"}, 
-    {"name": "Wallis and Futuna", "code": "WF"}, 
-    {"name": "Western Sahara", "code": "EH"}, 
-    {"name": "Yemen", "code": "YE"}, 
-    {"name": "Zambia", "code": "ZM"}, 
-    {"name": "Zimbabwe", "code": "ZW"} 
-];
 
+"use strict";
+////////////////  POP  UP MODAL AND DISPLAY OF PRODUCT IMAGES IN MODAL VIA LOOP
+let modal = document.querySelector('.modal');
+let expandItem = document.querySelectorAll('.fa-expand-arrows-alt');
+let collapse = document.querySelector('.close');
+
+let moreImages = document.querySelector('.images-container .more-images');
+let images = moreImages.getElementsByTagName('img');
+let displayArea = document.querySelector('.viewArea img');
+const cartSection = document.querySelector('[data-cart--session]');
+const toggleCart = document.querySelectorAll('.toggle-cart');
+let displayCartItems = document.querySelector('.item-list');
+const clearCartBtn = document.querySelector('#clear-cart');
+
+    
+let itemArray =[];
+let cart = [];
+
+class FetchData {
+    async getProducts() {
+        const apiKey = "./products.json";
+        try {
+            let result = await fetch(`${apiKey}`)
+            const data = await result.json();
+            let products = data.products;
+            products = products.map(product => {
+                // const id = products.id;
+                const categories = [...product.categories];
+                const {productName, price, id } = product;
+                const {thumbnail, others} = product.images
+                return {id, categories, thumbnail, others, productName, price}
+            });
+            console.log(products);
+            return products
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+class UI extends FetchData {
+
+    displayProducts(products) {
+        console.log(products)
+        let productsDOM = document.querySelector('.shop_menu');
+        products.forEach(product => {
+            productsDOM.innerHTML += this.productTemplate(product);
+        });
+    }
+
+    productTemplate( product ) {
+        let result = '';
+        const {id, categories, thumbnail, others, productName, price} = product;
+        result += `
+        <div class="shop_item" data-id="${id}" data-category="${[...categories]}">
+            <div class="product-image">
+                <img id="thumbnail" src="${thumbnail}" alt="">
+                <div class="more-images">
+                    ${others.map(imageUrl => `<img src="${imageUrl}" alt="">`)}
+                </div>
+                <div class="product-nav-btns">
+                    <i class="fas fa-shopping-cart cartBtn" data-id = ${id}></i></i>
+                    <i class="fas fa-expand-arrows-alt" data-id = ${id}></i>
+                </div>
+            </div>
+            <div class="product-info">
+                <a href="#"><h4 class="product-name">${productName}</h4></a>
+                <h5 class="price">$${price}</h5>
+            </div>
+        </div>
+        `
+        return result
+    }
+
+    cartItemTemplate(item) {
+        const div = document.createElement('div');
+        div.classList.add('item_in_cart');
+        const {thumbnail, id, productName, price, qty} = item
+        div.innerHTML = `
+            <li class="item_in_cart" data-id="${id}">
+                <div class="item_details">
+                    <img src="${thumbnail}" alt="" class="item_img">
+                    <div class="item_pro">
+                        <h3 class="product_name">${productName}</h3>
+                        <span class="price">Price: $${price}</span>
+                        <span class="item_total_price">Total:  ${price}</span>
+                    </div>
+                </div>
+
+                <div class="qty">
+                    <i class="fas fa-minus"></i>
+                    <span class="value">${qty}</span>
+                    <i class="fas fa-plus"></i>
+                </div>
+                <i class="far fa-trash-alt remove-item"></i>
+            </li>;
+        `
+        console.log(div);
+        return div
+    }
+
+    static findProduct(products, id) {
+        const product = products.find(product => product.id === id);
+        return product
+    }
+
+    addToCart() {
+        const cartBtns = [...document.querySelectorAll('.cartBtn')];
+        cartBtns.forEach(btn => {
+            const id = btn.dataset.id;
+            let product = cart.find(item => item.id == id);
+            console.log(product)
+            if(product){
+                btn.disabled = true;
+            }
+            else {
+                btn.addEventListener('click', event => {
+                    event.target.disabled;
+                    //get produt
+                    let cartItem = {...LocalStorage.getProduct(id), qty: 1};
+                    console.log(cartItem);
+
+                    //add product to cart
+                    cart = [...cart, cartItem];
+                    console.log(cart)
+                    //save Cart to LocalStorage
+                    LocalStorage.saveCart(cart);
+
+                    //update the cart
+                    this.updateCartValue(cart);
+
+                    //display cart items
+                    displayCartItems.appendChild(this.cartItemTemplate(cartItem));
+
+                    //Show Cart
+                    this.slideSideNav('displayCart', cartSection)
+                })
+            }
+        })
+        
+    }
+
+    slideSideNav(active, nav, btn) {
+        if(btn) {
+            if(btn.classList.contains('close')){
+                 nav.classList.remove(active);
+                 document.body.classList.remove('transparentBg')
+            }
+        }
+        nav.classList.add(active);
+        document.body.classList.add('transparentBg')
+    }
+
+    updateCartValue = (cart) =>  {
+        let overallBill = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            overallBill += item.price * item.qty;
+            itemsTotal += item.qty;
+        });
+        let cartTotal= document.querySelector('#total-amount');
+        cartTotal.textContent = `Total: $${parseFloat(overallBill.toFixed(2))}`;
+        document.querySelectorAll('#itemCounter').forEach(el => {
+            el.textContent = itemsTotal;
+        });
+    };
+
+    setUpAPP() {
+        cart = LocalStorage.getCart();
+        this.updateCartValue(cart);
+        cart.forEach(item => {
+            displayCartItems.appendChild(this.cartItemTemplate(item));
+        })
+    }
+
+    cartLogic(){
+        clearCartBtn.addEventListener("click", ()=> this.clearCart());
+
+        //Cart Functionalities
+
+        displayCartItems.addEventListener("click", event => {
+            const target = event.target;
+            if(target.classList.contains('remove-item')) {
+                const cartItem = target.parentElement.parentElement;
+                const id = target.parentElement.dataset.id;
+                displayCartItems.removeChild(cartItem);
+                this.removeItem(id);
+            }else if(target.classList.contains('fa-plus')) {
+                
+            }
+        })
+    }
+
+    clearCart() {
+        let cartItems = cart.map(item => item.id);
+        console.log(cartItems);
+        cartItems = cartItems.forEach(id => this.removeItem(id));
+        while(displayCartItems.children.length > 0) {
+            displayCartItems.removeChild(displayCartItems.children[0])
+        }
+    }
+
+    removeItem(id){
+        cart = cart.filter(item => item.id !== id);
+        this.updateCartValue(cart);
+        LocalStorage.saveCart(cart);
+        let btn = this.restoreCartBtn(id);
+        btn.disabled = false;
+        console.log(btn.disabled);
+    }
+
+    restoreCartBtn(id) {
+        const cartBtns = [...document.querySelectorAll('.cartBtn')];
+        return cartBtns.find(btn => btn.dataset.id === id);
+    }
+   
+    
+   
+}
+
+
+class LocalStorage {
+    static products(items) {
+        localStorage.setItem("Products", JSON.stringify(items))
+    }
+
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem("Products"));
+        return (
+            UI.findProduct(products, id)
+        )
+    }
+
+    static saveCart(cart) {
+        localStorage.setItem('Cart', JSON.stringify(cart))
+    }
+
+    static getCart() {
+        return localStorage.getItem("Cart") ? (
+             JSON.parse(localStorage.getItem("Cart"))) : []
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const ui = new UI;
+    const fetchData = new FetchData;
+    //Get all products
+    fetchData.getProducts()
+    .then(products => {
+            ui.displayProducts(products);
+            LocalStorage.products(products);
+        })
+    .then(()=> {
+        ui.uiCanInteract()
+    })
+
+});
+
+function toggle() {
+    var humburger = document.getElementById('navlinks');
+    if (humburger.style.display === 'block') {
+        humburger.style.display = 'none';
+    }
+    else {
+        humburger.style.display = 'block';
+    }
+}
 
